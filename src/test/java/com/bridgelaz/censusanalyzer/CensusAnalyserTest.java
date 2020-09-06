f110c8e027adacae513f6549dd9ec1f39b7d5707
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-06 23:24:10 +0530

    Added: Two files for checking wrong type and wrong delimiter

--- src/test/java/com/bridgelaz/censusanalyzer/CensusAnalyserTest.java
+++ src/test/java/com/bridgelaz/censusanalyzer/CensusAnalyserTest.java
@@ -9,7 +9,8 @@ import org.junit.rules.ExpectedException;
 public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = ".\\src\\test\\resources\\IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = ".\\src\\main\\resources\\IndiaStateCensusData.csv";
-
+    private static final String WRONG_CSV_FILE_TYPE_PATH = ".\\src\\test\\resources\\file.txt";
+    private static final String WRONG_DELIMITER_FILE_PATH = ".\\src\\test\\resources\\WrongDelimiter.csv";
     @Test
     public void givenIndianCensusCsvFile_ReturnCorrectRecords() {
         try {
@@ -38,7 +39,7 @@ public class CensusAnalyserTest {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(CensusAnalyserException.class);
-            censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_TYPE_PATH);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.FILE_TYPE_OR_DELIMITER_PROBLEM, e.type);
         }
@@ -50,7 +51,7 @@ public class CensusAnalyserTest {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(CensusAnalyserException.class);
-            censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            censusAnalyser.loadIndiaCensusData(WRONG_DELIMITER_FILE_PATH);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.FILE_TYPE_OR_DELIMITER_PROBLEM, e.type);
         }
